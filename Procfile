f87c80f29ff8198ecbe774d23927be3cdcd88b50
michael woo <mwoo123@gmail.com>
2016-04-26 21:05:44 -0400

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node anibot.js
---
+++ anibot.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let util = require('util');
+let http = require('http');
+let Bot  = require('@kikinteractive/kik');
+
+// Configure the bot API endpoint, details for your bot
+let bot = new Bot({
+    username: 'anibot',
+    apiKey: '90b0e4fd-177a-4610-ab20-efa3684be264',
+    baseUrl: 'https://anibot.heroku.com'
+});
+
+bot.updateBotConfiguration();
+
+bot.onTextMessage((message) => {
+    message.reply(message.body);
+});
+
+// Set up your server and start listening
+let server = http
+    .createServer(bot.incoming())
+    .listen(process.env.PORT || 8080);
