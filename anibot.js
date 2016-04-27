066bd76079d79f2276f7550ea3dd46a2c5b5e666
mwoo <mwoo123@gmail.com>
2016-04-27 17:48:17 -0400

    transition to desktop

--- anibot.js
+++ anibot.js
@@ -8,12 +8,54 @@ var request = require('request');
 // anilist oauth
 var ani_client_id = "mwoo-8zevs";
 var ani_client_secret = "PVbpoDC2My1xqyuL5OHK";
-var ani_endpoint = "https://anilist.co/api/"
-var ani_refresh = {
-  grant_type: "client_credentials",
-  client_id: ani_client_id,
-  client_secret: ani_client_secret
-};
+var ani_endpoint = "https://anilist.co/api/";
+var ani_token = '';
+
+function getNewAniToken(){
+  var ani_refresh = {
+    grant_type: "client_credentials",
+    client_id: ani_client_id,
+    client_secret: ani_client_secret
+  };
+
+  request.post({
+    url: ani_endpoint+'auth/access_token',
+    json: ani_refresh
+    },
+    function (error, response, body) {
+      //Check for error
+      if(error){
+          return console.log('Error:', error);
+      }
+
+      //Check for right status code
+      if(response.statusCode !== 200){
+          return console.log('Invalid Status Code Returned:', response.statusCode);
+      }
+
+      //All is good. Print the body
+      var jbody = JSON.parse(body);
+      ani_token = jbody['access_token'];
+    }
+  )
+}
+
+function searchAnime(name, attempt){
+  request(ani_endpoint+'anime/search/'+name+'?access_token='+token, function(error, response, body){
+    if(error){
+      console.log('Error:', error);
+      if(response.statusCode == 401){
+
+      }
+    }
+
+    if(response.statusCode !== 200){
+      if(response.statusCode == 401){
+
+      }
+    }
+  })
+}
 
 // kikbot auth
 var botname = 'anibot';
@@ -51,27 +93,6 @@ request.post({
     console.log(body); // Show the HTML for the Modulus homepage.
   }
 );
-
-request.post({
-  url: ani_endpoint+'auth/access_token',
-  json: ani_refresh
-  },
-  function (error, response, body) {
-    //Check for error
-    if(error){
-        return console.log('Error:', error);
-    }
-
-    //Check for right status code
-    if(response.statusCode !== 200){
-        return console.log('Invalid Status Code Returned:', response.statusCode);
-    }
-
-    //All is good. Print the body
-    console.log('success');
-    console.log(body); // Show the HTML for the Modulus homepage.
-  }
-)
 
 // Configure the bot API endpoint, details for your bot
 let bot = new Bot(botsettings);
