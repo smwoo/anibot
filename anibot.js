c06fa4ec2bb1316e2444de7f95a5f5b82bb9555f
michael woo <mwoo123@gmail.com>
2016-05-02 21:03:40 -0400

    add default response

--- anibot.js
+++ anibot.js
@@ -161,7 +161,7 @@ bot.onTextMessage((message) => {
 
 		}
 		else{
-			var reply = new Message.text();
+			var reply = Bot.Message.text();
 			reply.setBody("Sorry i didn't get that, please tell me your request");
 			var keyboardsuggestions = [{"type":"text",
 									 						    "body":"view and subscribe to the airing season"},
