3951eb7a32ed8c5329144ca0215c46585c5d4c6f
michael woo <mwoo123@gmail.com>
2016-05-20 13:38:26 -0400

    view airing

--- anibot.js
+++ anibot.js
@@ -158,7 +158,7 @@ bot.onTextMessage((message) => {
 				var animeCollection = db.collection('airing');
 				animeCollection.find().sort({'title': 1}).toArray(function(err, animearray){
 					var reply = Bot.Message.text();
-					reply.setBody(String(animearray[0]+'\n'+animearray[1]+'\n'+animearray[2]+'\n'+animearray[3]+'\n'+animearray[4]));
+					reply.setBody(animearray[0]['title']+'\n'+animearray[1]['title']+'\n'+animearray[2]['title']+'\n'+animearray[3]['title']+'\n'+animearray[4]['title']);
 					var keyboardsuggestions = ["next page", animearray[0]['title'], animearray[1]['title'], animearray[2]['title'], animearray[3]['title'], animearray[4]['title']]
 					reply.addResponseKeyboard(keyboardsuggestions, false, message.from);
 					bot.send([reply], message.from);
