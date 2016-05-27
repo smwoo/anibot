fd52cf3c6cb442f3fe2ebc56c404465e9588b3fa
michael woo <mwoo123@gmail.com>
2016-05-27 15:49:40 -0400

    remove finished shows

--- anibot.js
+++ anibot.js
@@ -338,7 +338,7 @@ bot.onTextMessage((message) => {
 						bot.send([reply], message.from);
 						conversationCollection.updateOne({'name':message.from},{$set:{'timestamp':Date.now(), 'state':'default'}});
 					}
-					else{
+					else if(searchresults > 1){
 						var keyboardsuggestions = [];
 						for(var i = 0; i < searchresults.length; i++){
 							if(i < 20){
@@ -351,6 +351,14 @@ bot.onTextMessage((message) => {
 						reply.addResponseKeyboard(keyboardsuggestions, false, message.from);
 						bot.send([reply], message.from);
 						conversationCollection.updateOne({'name':message.from},{$set:{'timestamp':Date.now(), 'state':'view'}});
+					}
+					else{
+						var reply = Bot.Message.text();
+						reply.setBody("No results found. What would you like to do?");
+						var keyboardsuggestions = ["view and subscribe to the airing season", "search anime"];
+						reply.addResponseKeyboard(keyboardsuggestions, false, message.from);
+						bot.send([reply], message.from);
+						conversationCollection.updateOne({'name':message.from},{$set:{'timestamp':Date.now()}});
 					}
 				}
 			});
