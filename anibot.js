faf54d625675eb61fc86a733541c9bc2a6e949a1
michael woo <mwoo123@gmail.com>
2016-05-27 16:44:21 -0400

    remove finished shows

--- anibot.js
+++ anibot.js
@@ -337,7 +337,10 @@ bot.onTextMessage((message) => {
 				if(response.statusCode == 400){
 					console.log('error from anime: '+anime['title']);
 				}
-				if(response.statusCode == 200 && body !== ''){
+				if(body == ''){
+					console.log('hahahaha');
+				}
+				if(body !== ''){
 					console.log("error "+body);
 					var searchresults = JSON.parse(body);
 					if(searchresults.length == 1){
